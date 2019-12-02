994e8459805b0574f225d3d8d3e479f351abc722
Xiao-Ze-Quan <1771434578@qq.com>
2019-12-02 16:12:50 +0800

    顶顶顶

--- src/com/inid/jack/controller/test.java
+++ src/com/inid/jack/controller/test.java
@@ -3,5 +3,6 @@ package com.inid.jack.controller;
 public class test {
     public static void main(String[] args) {
         System.out.println("dsfdsfds");
+        System.out.println("dsfdsfdsfds");
     }
 }
