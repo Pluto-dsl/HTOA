ad436600b72de4994e69f0826785360e75d77be6
Xiao-Ze-Quan <1771434578@qq.com>
2019-12-03 21:17:11 +0800

    员工流程图完成

--- src/com/inid/jack/controller/Test1.java
+++ src/com/inid/jack/controller/Test1.java
@@ -4,5 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/test1")
 public class Test1 {
+
+    @RequestMapping("/toTest1")
+    public String toTest1(){
+        return "test1";
+    }
+
 }
