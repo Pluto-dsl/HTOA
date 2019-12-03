c2f4bef760337565a47bfef6525236f518d118c2
norman <2583866825@qq.com>
2019-12-03 15:31:10 +0800

    李家伟新增了通知公告的vo层

---
+++ src/com/publics/vo/notice/NoticeVo.java
@@ -0,0 +1,13 @@
+package com.publics.vo.notice;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * 通知公告管理
+ *  公告通知明细表
+ * */
+@Entity
+@Table(name = "notice")
+public class NoticeVo {
+}
