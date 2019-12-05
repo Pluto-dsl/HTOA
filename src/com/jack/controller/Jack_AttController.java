0419a5eef31580a0eb0af9a8e84d294e871dd482
Xiao-Ze-Quan <1771434578@qq.com>
2019-12-05 08:14:34 +0800

    xzq

--- src/com/jack/controller/Jack_AttController.java
+++ src/com/jack/controller/Jack_AttController.java
@@ -3,6 +3,7 @@ package com.jack.controller;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.jack.service.AttService;
+import com.publics.vo.empModel.AttendanceVo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,6 +27,17 @@ public class Jack_AttController {
         return "emp_xzq/AttendancePage";
     }
 
+    @RequestMapping("/test")
+    public String test(){
+        return "emp_xzq/xxx";
+    }
+
+    @RequestMapping("/Attadd")
+    public String Attadd(AttendanceVo att,String timeing){
+        System.out.println(att);
+        System.out.println(timeing);
+        return "";
+    }
     @RequestMapping("/Att")
     @ResponseBody
     public void Att(HttpServletResponse response, HttpServletRequest request) throws IOException {
