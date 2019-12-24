38ea1bbd229d2301a9a257acd464536d6c7c30df
lucky-wtt <58408038+lucky-wtt@users.noreply.github.com>
2019-12-24 19:30:09 +0800

    更新

--- src/com/publics/service/impl/LoggingServiceImpl.java
+++ src/com/publics/service/impl/LoggingServiceImpl.java
@@ -4,12 +4,13 @@ import com.publics.dao.BaseDao;
 import com.publics.service.LoggingService;
 import com.publics.vo.empModel.emp.EmpVo;
 import com.publics.vo.sys.SystemLogVo;
+import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Date;
-
+@Service
 public class LoggingServiceImpl extends BaseDao implements LoggingService {
     @Override
     public void addLog(SystemLogVo log) {
